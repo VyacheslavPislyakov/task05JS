0d7f4c9e77294dad697af1def048afd583d985f1
Vyacheslav Pislyakov <Vyacheslav_Pislyakov@epam.com>
2017-07-14 08:14:43 +0300

    add func

--- conf.js
+++ conf.js
@@ -1,18 +1,20 @@
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: ['spec.js'],
-	// capabilities: {
-	// 	'browserName': 'firefox'
-	// },
+	capabilities: {
+		'browserName': 'chrome'
+	},
 	// onPrepare: function () { //Не работает на Firefox
 	// 	browser.driver.manage().window().maximize();
 	// },
 	//поставить несколько браузеров
-	multiCapabilities: [{
-		browserName: 'firefox'
-	}, {
-		browserName: 'chrome'
-	}]
+	// multiCapabilities: [{
+	// 	browserName: 'firefox'
+	// }, {
+	// 	browserName: 'chrome'
+	// }, {
+	// 	browserName: 'internet explorer'
+	// }]
 	// jasmineNodeOpts: {
 	// 	showColors: true,
 	// }
--- spec.js
+++ spec.js
@@ -1,31 +1,44 @@
 describe('angularjs homepage todo list', function() {
 
 	var findKittenLabel = element(by.xpath('//*[@id="gallery"]/section[2]/section[1]/section/ul/li[2]/h5'));
+
+	// beforeEach(() => {
 	browser.get('http://www.gettyimages.com/');
+	// });
+
 
 	it('go to creative page', function() {
-		// browser.get('http://www.gettyimages.com/');
 		element(by.xpath('//*[@id="site-header"]/div[2]/nav[1]/ul/li[1]/a')).click();
 	});
 
-	it('should have a page title', function () {
+	it('should have a page title', function() {
+
 		expect(browser.getTitle()).toEqual('Creative Images | Stock Photos, Illustrations, Vector Art, Photo Archives | Getty Images');
 	});
 
-	it('should have family pictures and count them', function () {
+	it('should have family pictures and count them', function() {
 		element(by.id('search-phrase')).sendKeys('family');
-		element(by.xpath('//*[@id="search-container-fancy"]/form/fieldset/button/img')).click();
+		element(by.xpath('//*[@id="search-container-fancy"]/form/fieldset/button/img')).click(); //не отрабатывает клик
 		expect(element.all(by.css('.mosaic-asset')).count()).toBeLessThanOrEqual(60);
+		browser.sleep(3000);
 	});
 
-	it('go back and show puppies pictures', function () {
-		browser.navigate().back();
-		element(by.id('search-phrase')).sendKeys('puppies');
-		element(by.xpath('//*[@id="search-container-fancy"]/form/fieldset/button/img')).click();
+	it('test executors', function() {
+		var el = element(By.id('logo'));
+		browser.executeScript("arguments[0].style.backgroundColor = '" + "yellow" + "'", el);
+		browser.sleep(3000);
+	});
+
+	it('go back and show puppies pictures', function() {
+		// browser.navigate().back();
+		browser.get('http://www.gettyimages.com/creative-images');
+		var search = element(by.id('search-phrase')).sendKeys('puppies');
+		search.sendKeys(protractor.Key.ENTER).perform;
+		// element(by.xpath('//*[@id="search-container-fancy"]/form/fieldset/button/img')).click();
 
 	});
 
-	it('should have Related searches', function () {
+	it('should have Related searches', function() {
 		expect(findKittenLabel.getText()).toEqual('Related searches');
 	});
 
